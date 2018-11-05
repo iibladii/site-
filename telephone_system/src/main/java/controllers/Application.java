0d47d6785ae146aa34bbee5230b3ebc92babcadb
Mokshantsev Vladislav <super.blad2011@yandex.ru>
2018-11-05 13:14:06 +0500

    # WARNING: head commit changed in the meantime
    fgh

--- telephone_system/src/main/java/controllers/Application.java
+++ telephone_system/src/main/java/controllers/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class Application {
-
+//https://www.youtube.com/watch?v=n7k20TTfPOI
     public static void main(String[] args) throws Throwable {
         SpringApplication.run(Application.class, args);
     }
